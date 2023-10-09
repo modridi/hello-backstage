7bb7e42f2249bb22dc47cfa1378b72efcd169618
Mohamed Amine Dridi <78367821+modridi@users.noreply.github.com>
2023-10-09 11:49:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 8080
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello Camptocamp!');
+  res.end('Hello Camptocamp! lets try again');
 }
 const server = http.createServer(requestListener);
 
